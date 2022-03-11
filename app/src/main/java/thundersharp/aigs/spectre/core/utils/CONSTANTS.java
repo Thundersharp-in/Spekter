ee0d09c8c98476ee4bed7221e999304278a6734c
hrishikeshprateek <prateekhrishikesh@gmail.com>
2022-03-11 16:07:20 +0530

    Improved ExhibitionHome page layout

--- app/src/main/java/thundersharp/aigs/spectre/core/utils/CONSTANTS.java
+++ app/src/main/java/thundersharp/aigs/spectre/core/utils/CONSTANTS.java
@@ -3,4 +3,5 @@ package thundersharp.aigs.spectre.core.utils;
 public class CONSTANTS {
     public static final String DATABASE_TOP_CAROUSEL = "TOP_CAROUSEL";
     public static final String DATABASE_NODE_USERS = "USERS";
+    public static final String DATABASE_EXHIBITION_SLIDER = "EXHIBITION_SLIDER";
 }
--- app/src/main/java/thundersharp/aigs/spectre/ui/activities/ExhibitionHome.java
+++ app/src/main/java/thundersharp/aigs/spectre/ui/activities/ExhibitionHome.java
@@ -1,5 +1,6 @@
 package thundersharp.aigs.spectre.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.widget.NestedScrollView;
@@ -11,15 +12,29 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.bumptech.glide.request.RequestOptions;
+import com.glide.slider.library.SliderLayout;
+import com.glide.slider.library.animations.DescriptionAnimation;
+import com.glide.slider.library.slidertypes.BaseSliderView;
+import com.glide.slider.library.slidertypes.DefaultSliderView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import thundersharp.aigs.spectre.R;
 import thundersharp.aigs.spectre.core.adapters.ProjectsHolderAdapter;
 import thundersharp.aigs.spectre.core.models.ProjectShortDescription;
+import thundersharp.aigs.spectre.core.models.SliderModel;
+import thundersharp.aigs.spectre.core.utils.CONSTANTS;
+import thundersharp.aigs.spectre.ui.fragments.home.HomeFragment;
 
-public class ExhibitionHome extends AppCompatActivity {
+public class ExhibitionHome extends AppCompatActivity implements BaseSliderView.OnSliderClickListener {
 
+    private SliderLayout slider;
     private AppCompatButton book;
     private NestedScrollView mainContents;
     private RelativeLayout preAnimation;
@@ -34,10 +49,12 @@ public class ExhibitionHome extends AppCompatActivity {
         book = findViewById(R.id.book);
         mainContents = findViewById(R.id.mainScroll);
         preAnimation = findViewById(R.id.mainContainer);
+        slider = findViewById(R.id.slider);
 
         recyclerProjects = findViewById(R.id.recyclerProjects);
 
         setPreAnimation(true);
+        setupCrousel();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -71,4 +88,89 @@ public class ExhibitionHome extends AppCompatActivity {
             preAnimation.setVisibility(View.GONE);
         }
     }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+
+
+    private void setupCrousel(){
+        ArrayList<String> listUrl = new ArrayList<>();
+        Bundle bundle = new Bundle();
+
+        FirebaseDatabase
+                .getInstance()
+                .getReference(CONSTANTS.DATABASE_EXHIBITION_SLIDER)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            RequestOptions requestOptions = new RequestOptions();
+                            requestOptions.centerCrop();
+                            for (DataSnapshot dataSnapshot :snapshot.getChildren()){
+
+                                DefaultSliderView sliderView = new DefaultSliderView(ExhibitionHome.this);
+                                sliderView
+                                        .image(dataSnapshot.child("URL").getValue(String.class))
+                                        .description(null)
+                                        .setRequestOption(requestOptions)
+                                        .setProgressBarVisible(true)
+                                        .setOnSliderClickListener(ExhibitionHome.this);
+
+                                //add your extra information
+                                SliderModel model = dataSnapshot.getValue(SliderModel.class);
+                                Bundle bundle1 = new Bundle();
+                                bundle1.clear();
+                                bundle1.putSerializable("data", model);
+                                //bundle1.putInt("PAGE",dataSnapshot.child("PAGE").getValue(Integer.class));
+                                sliderView.bundle(bundle1);
+
+                                slider.addSlider(sliderView);
+                            }
+
+                        }else {
+                            listUrl.add("https://images.unsplash.com/photo-1582513166998-56ed1ea02d13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80");
+                            listUrl.add("https://images.unsplash.com/photo-1507413245164-6160d8298b31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80");
+
+                            RequestOptions requestOptions = new RequestOptions();
+                            requestOptions.centerCrop();
+
+                            for (int i = 0; i < listUrl.size(); i++) {
+                                DefaultSliderView sliderView = new DefaultSliderView(ExhibitionHome.this);
+                                // if you want show image only / without description text use DefaultSliderView instead
+
+                                // initialize SliderLayout
+                                sliderView
+                                        .image(listUrl.get(i))
+                                        .description(null)
+                                        .setRequestOption(requestOptions)
+                                        .setProgressBarVisible(true)
+                                        .setOnSliderClickListener(ExhibitionHome.this);
+
+                                //add your extra information
+                                sliderView.bundle(bundle);
+
+                                slider.addSlider(sliderView);
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        slider.setPresetTransformer(SliderLayout.Transformer.ZoomOutSlide);
+
+        slider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        slider.setCustomAnimation(new DescriptionAnimation());
+        slider.setDuration(4000);
+        //slider.addOnPageChangeListener(Content_home.this);
+        slider.stopCyclingWhenTouch(false);
+    }
+
 }
