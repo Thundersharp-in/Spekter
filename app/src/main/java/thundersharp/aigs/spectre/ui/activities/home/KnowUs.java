99ee2e21e66f0df114dab081cca6837a5022dbc3
hrishikeshprateek <prateekhrishikesh@gmail.com>
2022-04-19 23:04:32 +0530

    #4.2 complete implementation and Bug Fixes in WorkshopDetails

--- app/src/main/java/thundersharp/aigs/spectre/ui/activities/home/KnowUs.java
+++ app/src/main/java/thundersharp/aigs/spectre/ui/activities/home/KnowUs.java
@@ -3,15 +3,38 @@ package thundersharp.aigs.spectre.ui.activities.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import thundersharp.aigs.spectre.R;
 
 public class KnowUs extends AppCompatActivity {
 
+    private RelativeLayout loader,mainContents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_know_us);
+
+        loader = findViewById(R.id.container);
+        mainContents = findViewById(R.id.mainContents);
+
+        setPreAnimation(true);
+
+        new Handler().postDelayed(() -> setPreAnimation(false),2000);
+
+    }
+
+    private synchronized void setPreAnimation(boolean animation) {
+        if (animation) {
+            mainContents.setVisibility(View.GONE);
+            loader.setVisibility(View.VISIBLE);
+        } else {
+            mainContents.setVisibility(View.VISIBLE);
+            loader.setVisibility(View.GONE);
+        }
     }
 
 }
--- app/src/main/java/thundersharp/aigs/spectre/ui/activities/home/UpcommingEventsHome.java
+++ app/src/main/java/thundersharp/aigs/spectre/ui/activities/home/UpcommingEventsHome.java
@@ -3,14 +3,38 @@ package thundersharp.aigs.spectre.ui.activities.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import thundersharp.aigs.spectre.R;
 
 public class UpcommingEventsHome extends AppCompatActivity {
 
+    private RelativeLayout contents,loader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcomming_events_home);
+
+        contents = findViewById(R.id.contents);
+        loader = findViewById(R.id.loader);
+
+        setPreAnimation(true);
+
+        new Handler().postDelayed(() -> {
+            setPreAnimation(false);
+        }, 2000);
+    }
+
+    private synchronized void setPreAnimation(boolean animation) {
+        if (animation) {
+            contents.setVisibility(View.GONE);
+            loader.setVisibility(View.VISIBLE);
+        } else {
+            contents.setVisibility(View.VISIBLE);
+            loader.setVisibility(View.GONE);
+        }
     }
 }
