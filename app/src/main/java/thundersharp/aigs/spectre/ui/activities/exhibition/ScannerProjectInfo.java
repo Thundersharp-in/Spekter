9e16da1ce0eb6a698629f553970ca7ddac729e5f
hrishikeshprateek <prateekhrishikesh@gmail.com>
2022-04-22 07:35:47 +0530

    #4.2 complete implementation and Bug Fixes in WorkshopDetails

--- app/src/main/java/thundersharp/aigs/spectre/ui/activities/exhibition/ScannerProjectInfo.java
+++ app/src/main/java/thundersharp/aigs/spectre/ui/activities/exhibition/ScannerProjectInfo.java
@@ -1,35 +1,19 @@
 package thundersharp.aigs.spectre.ui.activities.exhibition;
 
 import android.os.Bundle;
-import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ui.AppBarConfiguration;
 
-import com.google.android.material.snackbar.Snackbar;
-
-import thundersharp.aigs.spectre.databinding.ActivityScannerProjectInfoBinding;
+import thundersharp.aigs.spectre.R;
 
 
 public class ScannerProjectInfo extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityScannerProjectInfoBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityScannerProjectInfoBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        setSupportActionBar(binding.toolbar);
+        setContentView(R.layout.activity_scanner_project_info);
 
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
